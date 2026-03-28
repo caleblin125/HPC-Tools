597f1d3ef9543f9a058a182c936ed90525fdb5a5
Caleb Lin <caleblin@login07.chn.perlmutter.nersc.gov>
2026-03-27 22:12:15 -0700

    spack installation improvement

--- spack_install.sh
+++ spack_install.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SPACK_DIR=~/spack
+# Usage: ./install_spack.sh [install_dir]
+
+SPACK_DIR="${1:-$HOME}/spack"
+
 if [[ ! -d "$SPACK_DIR" ]]; then
-    git clone --depth=2 --branch=releases/v1.0 https://github.com/spack/spack.git $SPACK_DIR
-    cd ~/spack
+    git clone --depth=2 --branch=releases/v1.0 https://github.com/spack/spack.git "$SPACK_DIR"
+    
+    cd "$SPACK_DIR" || exit 1
     . share/spack/setup-env.sh
-    cd ~
-    echo "source ~/spack/share/spack/setup-env.sh" >> ~/.bashrc
-    echo "spack installed"
+    cd ~ || exit 1
+
+    # Only add to bashrc if not already present
+    if ! grep -q "spack/share/spack/setup-env.sh" ~/.bashrc; then
+        echo "source $SPACK_DIR/share/spack/setup-env.sh" >> ~/.bashrc
+    fi
+    source $SPACK_DIR/share/spack/setup-env.sh
+    echo "spack installed at $SPACK_DIR"
 else
-    echo "spack already exists"
-fi 
+    source $SPACK_DIR/share/spack/setup-env.sh
+    echo "spack already exists at $SPACK_DIR"
+fi
